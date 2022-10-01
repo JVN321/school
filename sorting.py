16bd7b92530e733c2b3e45c7e8bfb997e6ced806
Johnvarghesenettady <67957285+Johnvarghesenettady@users.noreply.github.com>
2022-10-01 22:01:34 +0530

    finally fixed stuff here

--- sorting.py
+++ sorting.py
@@ -1,4 +1,5 @@
 def sorting(filename):
+  name = filename.split("/")[1]
   infile = open(filename)
   words = []
   for line in infile:
@@ -7,9 +8,21 @@ def sorting(filename):
       words.append(i)
   infile.close()
   words.sort()
-  outfile = open("idsdone.txt", "w")
+  outfile = open("admissionsorted/" + name, "w+")
   for i in words:
     outfile.writelines(i)
     outfile.writelines("\n")
   outfile.close()
-sorting("ids.txt")
+
+
+sorting("admissionnumbers/bnvv.amserp.in.txt")
+'''
+sorting("admissionnumbers/bav.amserp.in.txt")
+sorting("admissionnumbers/bmv.amserp.in.txt")
+sorting("admissionnumbers/bmv.amserp.in.txt")
+sorting("admissionnumbers/bvme.amserp.in.txt")
+sorting("admissionnumbers/bvmer.amserp.in.txt")
+sorting("admissionnumbers/bvmg.amserp.in.txt")
+sorting("admissionnumbers/bvvt.amserp.in.txt")
+sorting("admissionnumbers/bnvv.amserp.in.txt")
+'''
